f39a0c9f8370fa027ec7aa1969f564ab44eb8a2b
Pnixys <julien.daugaronthe@gmail.com>
2021-12-14 15:09:56 +0100

    modification création database

--- config/creation_database.sql
+++ config/creation_database.sql
@@ -1,21 +1,21 @@
 CREATE DATABASE PostFlex;
 
 CREATE TABLE sprint (
-    id INT PRIMARY KEY NOT NULL PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY NOT NULL PRIMARY KEY,
     date_debut DATE,
     date_fin DATE
 );
 
 CREATE TABLE colonne (
-    id INT NOT NULL PRIMARY KEY,
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
     titre VARCHAR(255) NOT NULL,
     id_sprint INT NOT NULL,
     max_tache INT NOT NULL DEFAULT 10,
 );
 
 CREATE TABLE post_it (
-    id INT PRIMARY KEY NOT NULL PRIMARY KEY,
-    couleur VARCHAR(7),
+    id VARCHAR(255) PRIMARY KEY NOT NULL PRIMARY KEY,
+    couleur VARCHAR(255),
     description VARCHAR(255),
     date_creation DATE,
     estimation_temp INT NOT NULL
@@ -27,8 +27,8 @@ CREATE TABLE projet (
 );
 
 CREATE TABLE projets_with_users (
-    id_projet INT NOT NULL,
-    id_user INT NOT NULL
+    id_projet VARCHAR(255) NOT NULL,
+    id_user VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users (
